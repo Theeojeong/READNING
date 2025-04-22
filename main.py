c6fe87ee3397d86c17097e029042f105e44ed001
MINKI CHO <73434167+tidls1995@users.noreply.github.com>
2025-04-22 16:35:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -29,9 +29,13 @@ app.include_router(musicgen_upload_router.router)
 def root():
     return { "message": "Readning API is running" }
 
-@app.get("/download")
-def download_final_mix():
-    path = os.path.join(OUTPUT_DIR, FINAL_MIX_NAME)
+
+
+@app.get("/gen_musics/{book_id}/ch{page}.wav")
+def download_final_mix(book_id: str, page: int):
+    filename = f"ch{page}.wav"
+    path = os.path.join(OUTPUT_DIR, book_id, filename)
+
     if os.path.exists(path):
-        return FileResponse(path, filename = FINAL_MIX_NAME, media_type="audio/wav")
-    return {"error" : "최종 음원이 존재하지 않습니다."}
+        return FileResponse(path, filename=filename, media_type="audio/wav")
+    return {"error": f"최종 음원 파일이 존재하지 않습니다: {path}"}
